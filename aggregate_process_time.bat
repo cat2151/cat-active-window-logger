3989095d6e826044c67a53ead687d5534d46f531
cat2151 <fct_2004@mail.goo.ne.jp>
2025-09-20 23:16:47 +0900

    前処理漏れの修正

--- aggregate_process_time.bat
+++ aggregate_process_time.bat
@@ -1,4 +1,7 @@
 cd /d %~dp0
 set PYTHONPATH=%CD%\src
+
+call log_enhance_active_time.bat
+
 python -m src.log_processor.aggregate_process_time --config-filename secrets\aggregate_process_time.toml
 if not %errorlevel% == 0 (pause & exit /b 1)
